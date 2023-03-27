6afb459cc2aef7aea10ccca4c755b355b78967c1
tanaka <lemo.nade.engineer.room@gmail.com>
2023-03-27 11:39:01 +0900

    feat: #4 技術スタックのタスクのinterfaceを作成

---
+++ src/interfaces/scraping/topaz.ts
@@ -0,0 +1,18 @@
+import Skill from "~/interfaces/models"
+
+/** トパーズから技術スタックをスクレイピングして返す */
+export type TopazSkillsScraper = () => Promise<TopazSkills>
+
+export interface TopazSkills {
+    /** ジャンル: 言語 */
+    language: readonly Skill[]
+
+    /** ジャンル: フレームワーク */
+    framework: readonly Skill[]
+
+    /** ジャンル: データベース */
+    database: readonly Skill[]
+
+    /** ジャンル: その他 */
+    service: readonly Skill[]
+}
