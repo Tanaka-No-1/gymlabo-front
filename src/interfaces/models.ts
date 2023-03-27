2bc610f3df1c510fad3f4d55bc7bf104abf5ee0f
Yoichiro Tanaka <97539098+lemo-nade-room@users.noreply.github.com>
2023-03-27 11:40:52 +0900

    Revert "feat: #4 技術スタックのタスクのinterfaceを作成"

--- src/interfaces/models.ts
+++
@@ -1,7 +0,0 @@
-export default interface Skill {
-    /** 技術アイコンのURL */
-    icon: string
-
-    /** 技術名 */
-    name: string
-}
--- src/lib/topazScraper.ts
+++
@@ -1,8 +0,0 @@
-import { TopazSkillsScraper } from "~/interfaces/scraping/topaz"
-
-const topazScraper: TopazSkillsScraper = () => {
-    // リューちゃんはこの中身を作成する
-    return null as any
-}
-
-export default topazScraper
