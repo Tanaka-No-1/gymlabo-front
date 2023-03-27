58b1f9ab532eca07a9d5b7c858aec75bf3d4b42b
稲岡天駿 <heavenhayao2316@gmail.com>
2023-03-28 05:37:22 +0900

    add #32 chatgptでアイディアを挙げる関数の実装

--- src/lib/generateIdeas.ts
+++ src/lib/generateIdeas.ts
@@ -1,10 +1,40 @@
+import { Configuration, OpenAIApi } from 'openai'
 import GenerateIdeas from '~/interfaces/ai/generateIdeas'
 
 const generateIdeas: GenerateIdeas = async (skill) => {
-  // いなちゃんのタスク
-  // ChatGPT呼び出す
-  skill.database.join(','); // 配列から文字列に変換
-  return []
+  const l = skill.language.filter((_) => _).join(' ')
+  const f = skill.framework.filter((_) => _).join(' ')
+  const d = skill.database.filter((_) => _).join(' ')
+  const s = skill.service.filter((_) => _).join(' ')
+
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+  const openai = new OpenAIApi(configuration)
+
+  const content = `${l} ${f} ${d} ${s}の技術を使用。
+      これらを用いてハッカソンで作成するアプリを3つ以下のjson形式で出力してください。文字列は全て日本語で、各アプリの概要は15文字以内の日本語で記述する。
+      type string["アプリタイトル：アプリ概要",]`
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content,
+      },
+    ],
+  })
+
+  const array = completion.data.choices[0].message?.content
+    .replace('[', '')
+    .replace(/\n/g, '')
+    .replace(/ /g, '')
+    .replace(/"/g, '')
+    .replace(']', '')
+    .split(',')
+
+  return array ?? ['']
 }
 
 export default generateIdeas
