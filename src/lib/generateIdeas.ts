0d12a1f2d156214e45c36a2a124c5ec970b193e5
稲岡天駿 <heavenhayao2316@gmail.com>
2023-03-28 01:01:51 +0900

    Initial commit

--- src/lib/generateIdeas.ts
+++ src/lib/generateIdeas.ts
@@ -3,6 +3,7 @@ import GenerateIdeas from '~/interfaces/ai/generateIdeas'
 const generateIdeas: GenerateIdeas = async (skill) => {
   // いなちゃんのタスク
   // ChatGPT呼び出す
+  skill.database.join(','); // 配列から文字列に変換
   return []
 }
 
