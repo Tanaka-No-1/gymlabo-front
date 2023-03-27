079e671ce22fa72fa8625c7496db96c5b58808d1
tanaka <lemo.nade.engineer.room@gmail.com>
2023-03-27 11:40:11 +0900

    feat: #4 技術スタックのタスクのinterfaceを作成

---
+++ src/interfaces/models.ts
@@ -0,0 +1,7 @@
+export default interface Skill {
+  /** 技術アイコンのURL */
+  icon: string
+
+  /** 技術名 */
+  name: string
+}
---
+++ src/lib/topazScraper.ts
@@ -0,0 +1,8 @@
+import { TopazSkillsScraper } from '~/interfaces/scraping/topaz'
+
+const topazScraper: TopazSkillsScraper = () => {
+  // リューちゃんはこの中身を作成する
+  return null as any
+}
+
+export default topazScraper
