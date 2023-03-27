9de845d76d2d18fe99b415d20b6d9d510191d5b4
tanaka <lemo.nade.engineer.room@gmail.com>
2023-03-27 23:55:37 +0900

    feat: 🚧 #31 READMEを返すAPIの実装

---
+++ src/pages/api/readme/index.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { ReadmeRequestDataScheme } from '~/interfaces/api/readme/POSTRequest'
+import generateReadme from '~/lib/generateReadme'
+import ReadmeService from '~/service/readme/readmeService'
+
+const readmeService = new ReadmeService(generateReadme)
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  switch (req.method) {
+    case 'POST':
+      await POST(req, res)
+      return
+    default:
+      res.status(404).end(null)
+  }
+}
+
+async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const data = ReadmeRequestDataScheme.parse(req.body)
+  const text = await readmeService.generate(data.skillSet, data.idea)
+  res.status(200).json({ text })
+}
